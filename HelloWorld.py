69f1fc579fdc22179f451a35bbc2459d38a2aaff
chrismullavey <30323351+chrismullavey@users.noreply.github.com>
2017-08-01 17:11:55 -0400

    Hello World Script
    #WRITTEN IN PYTHON 3.6

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+#WRITTEN IN PYTHON 3.6
+print('Hello world!')
+print('What is your name?')
+myName=input()
+print('It is good to meet you, '+myName+'.')
+print('Your name is '+str(len(myName))+' characters long.')
+print('What is your age?')
+myAge=input()
+print('You will be '+str(int(myAge)+1)+' next year!')
